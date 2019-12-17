fd7f687eeed7dd2f288fd364d6a2346e7e75e618
summer9g <57480728+summer9g@users.noreply.github.com>
2019-12-17 10:20:04 +0700

    front end

---
+++ footer.php
@@ -0,0 +1,8 @@
+            <div id="address">
+                <div class="name">
+                    TRƯỜNG ĐẠI HỌC THỦY LỢI 
+                </div>
+                Địa chỉ : 175 TÂY SƠN, ĐỐNG ĐA, HÀ NỘI. <br>
+                Điện thoại: (024) 3852 2201 - Fax: (024) 3563 3351
+                Email: <a href="#">phonghcth@tlu.edu.vn</a>
+            </div>
---
+++ header.php
@@ -0,0 +1,16 @@
+        <div class="header-left">
+                <a href="#"><img src="images/logo.png" alt="logo"></a>
+            </div>
+            <div class="header-right">
+                <div class="login">
+                    <table>
+                        <tr>
+                            <td>Tài khoản: </td>
+                            <td><input type="text" style="height: 18px; width: 100px;" ></td>
+                            <td>Mật khẩu: </td>
+                            <td><input type="password" style="height: 18px; width: 100px;"></td>
+                            <td><button type="submit" style="height: 30px; font-size: medium;">Đăng nhập</button></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
